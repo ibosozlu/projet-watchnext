244c5ea4713a19d1dcff185ae86a07d443235393
Julien Calcerrada <39770259+Julien-Clrd@users.noreply.github.com>
2018-08-20 15:44:40 +0200

    Modifs

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,4 +14,12 @@ class IndexController extends AbstractController
     {
         return $this->render('index/index.html.twig');
     }
+
+    /**
+     * @Route("/recommandation")
+     */
+    public function recommandation()
+    {
+        return $this->render('page/recommandation.html.twig');
+    }
 }
